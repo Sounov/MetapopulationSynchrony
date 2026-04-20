c3a816dd18ea8e236c8681452ab90e0be046ffcb
Sounov <100077720+Sounov@users.noreply.github.com>
2026-04-20 20:01:12 +0530

    Delete Spatiotemporal frequency plot - Copy directory

--- Spatiotemporal frequency plot - Copy/extinction.m
+++
@@ -1,8 +0,0 @@
-function numvec = extinction(vec)
-%% Nonzero elements in a row=======================
-[m,n]=size(vec);
-numvec = zeros(n,1);
-for i=1:1:n
-    numvec(i,1) = m-nnz(vec(:,i));
-end
-end
